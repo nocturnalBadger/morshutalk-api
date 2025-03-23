5b378c22242298b4b27ea2d0903f32ba7f602f58
Jeremy Schmidt <8888694+nocturnalBadger@users.noreply.github.com>
2025-03-22 22:45:31 -0400

    add video support

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ LABEL org.opencontainers.image.source=https://github.com/nocturnalBadger/morshut
 
 RUN apt update && apt install -y ffmpeg && rm -rf /var/lib/apt/lists/*
 
-RUN pip install morshutalk uvicorn[standard] fastapi cryptography
+RUN pip install morshutalk uvicorn[standard] fastapi cryptography moviepy
 RUN python -c 'import nltk; nltk.download("averaged_perceptron_tagger_eng")'
 
 WORKDIR /opt/morshu
 COPY morshuapi.py  /opt/morshu/morshuapi.py
+COPY assets /opt/morshu/assets
 
 CMD python morshuapi.py
 
--- morshuapi.py
+++ morshuapi.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI, Response, HTTPException
-from fastapi.responses import StreamingResponse
-from morshutalk import morshu
-from pydantic import BaseModel
-from enum import Enum
 import io
 import tempfile
 import os
 import ssl
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from enum import Enum
 
-
-app = FastAPI()
-morsher = morshu.Morshu()
+from fastapi import FastAPI, Response, HTTPException
+from fastapi.responses import StreamingResponse
+from morshutalk.morshu import Morshu
+from pydantic import BaseModel
+from pydub import AudioSegment
+from moviepy import ImageSequenceClip, AudioArrayClip, AudioFileClip
 
 MAX_SIZE = 1000
+FRAME_RATE = 60
+SECS_PER_FRAME = (1.0 / FRAME_RATE)
+
+app = FastAPI()
+
+master_video_clip = ImageSequenceClip("./assets", fps=FRAME_RATE)
+master_video_frames = [frame for frame in master_video_clip.iter_frames()]
+
 
 class MorshResponseType(Enum):
     AUDIO = "audio"
@@ -26,20 +35,72 @@ class MorshingRequest(BaseModel):
 def get_root():
     return "Hello World"
 
+def construct_video(morsher: Morshu, audio: AudioSegment, output_path: str):
+
+    output_frames = []
+    output_frame_indices = []
+    output_durations = []
+    time_offset = 0.0
+    millis_per_frame = FRAME_RATE
+    while time_offset + SECS_PER_FRAME < audio.duration_seconds:
+        frame_idx = morsher.get_frame_idx_from_millis(time_offset * 1000)
+        if len(output_frame_indices) == 0:
+            output_frame_indices.append(frame_idx)
+        if not output_durations:
+            output_durations.append(0)
+        if frame_idx == output_frame_indices[-1]:
+            output_durations[-1] += SECS_PER_FRAME
+        else:
+            output_frame_indices.append(frame_idx)
+            output_durations.append(SECS_PER_FRAME)
+
+        time_offset += SECS_PER_FRAME
+
+    video_length_mismatch = sum(output_durations) - audio.duration_seconds
+    output_durations[-1] -= video_length_mismatch
+
+    output_frames = [master_video_frames[i] for i in output_frame_indices]
+    output_clip = ImageSequenceClip(output_frames, durations=output_durations)
+
+    with NamedTemporaryFile(suffix=".mp3") as audio_file:
+        audio.export(audio_file.name, format="mp3")
+
+        audio_clip = AudioFileClip(audio_file.name, fps=audio.frame_rate)
+        output_clip = output_clip.with_audio(audio_clip)
+
+    output_clip.write_videofile(output_path, fps=FRAME_RATE)
+
+
 @app.post("/morsh")
 async def get_morshed(morsh_req: MorshingRequest):
 
+    print(morsh_req)
     if len(morsh_req.message) > MAX_SIZE:
         return HTTPException(status_code=400, detail=f"message cannot be longer than {MAX_SIZE}")
 
+    morsher = Morshu()
+    morsh_audio = morsher.load_text(morsh_req.message)
+    morsh_audio = morsh_audio.append(AudioSegment.silent(duration=100))
 
-    def morshify(text: str):
-        audio = morsher.load_text(text)
+    def stream_audio(audio: AudioSegment):
         with io.BytesIO() as buffer:
             audio.export(buffer, format="mp3")
             yield from buffer
 
-    return StreamingResponse(morshify(morsh_req.message), media_type="audio/mp3")
+    def stream_video_file(file_path):
+        with open(file_path, "rb") as video_file:
+            yield from video_file
+        os.unlink(file_path)
+
+    if morsh_req.response_type == MorshResponseType.AUDIO:
+        return StreamingResponse(stream_audio(morsh_audio), media_type="audio/mp3")
+    elif morsh_req.response_type == MorshResponseType.VIDEO:
+        video_file = NamedTemporaryFile(suffix=".mp4", delete_on_close=False, delete=False)
+        construct_video(morsher, morsh_audio, video_file.name)
+
+        return StreamingResponse(stream_video_file(video_file.name), media_type="video/mp4")
+    else:
+        return HTTPException(status_code=400, detail=f"response type {morsh_req.response_type} is invalid")
 
 if __name__ == "__main__":
     import uvicorn
